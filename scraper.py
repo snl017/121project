f8e748956ceb1f2025733a106702af5a609b3708
Shannon Lubetich <shannonlubetich@gmail.com>
2014-04-17 23:10:17 -0700

    conversion from scraped hours into military format WORKS

--- scraper.py
+++ scraper.py
@@ -15,38 +15,67 @@ import string
 # for elt in test:
 # 	print(str(elt))
 
+
+#helper method for calculating military time
+#returns an ordered pair of strings
+#the first is a string of the hour digits (0-24)
+#the second elt is a string of the minute digits (0-59)
+def extractHoursMinutes(n):
+	if len(n) == 3 :
+		return (n[:1], n[1:])
+	elif len(n) == 4 :
+		return (n[:2], n[2:])
+	else :
+		return (n, "0")
+
 #Wrapper Function - put hours into database-friendly military time
 #Accepts a BeautifulSoup Object String
+#returns a string
 def military(n):
+	#make string from BeautifulSoup object
 	encoded = n.encode('utf-8')
+	#eliminate symobls from string
 	formatted = encoded.translate(None, ' .:')
+
+	#make unicode, eliminate spaces, then turn back into string
 	uniString = unicode(formatted, "UTF-8")
 	uniString = uniString.replace(u"\u00A0", "")
 	strFormat = str(uniString.encode('utf-8'))
 
-	#Replace Keywords
-	strFormat = strFormat.replace("midnight", "2400pm")
+	#convert to lowercase, replace Keywords
+	strFormat = strFormat.lower()
+	strFormat = strFormat.replace("midnight", "2400am")
 	strFormat = strFormat.replace("noon", "1200pm")
 
-	#Add 0s as necessary
-	#Parse first hours number via regex
+	#Using regex, get array of all groups of digits
 	numbersPattern = re.compile("\d+")
-	numbersArray = testPattern.findall(strFormat)
-	print collect
+	numbersArray = numbersPattern.findall(strFormat)
 
+	#Using regex, get array of all groups of letters (will be am/pm)
 	amPmPattern = re.compile("[a-zA-Z]+")
 	amPmArray = amPmPattern.findall(strFormat)
-	print amPmArray
 
-	#Checking for am and pm and adding accordingly
-	for i in range len(amPmArray):
-		if amPmArray[i] == "pm":
-			rawCorrespondingTime = numbersArray[i];
-			#parse it into integer
-			rawIntTime = (int(rawCorrespondingTime)%12) + 12
-
-
-	return strFormat
+	#go through all numbers, format correctly into 4-digit military time
+	formattedHours = []
+	for i in range(len(numbersArray)):
+		time = numbersArray[i]
+		hr_min = extractHoursMinutes(time)
+		hrDigits = int(hr_min[0])
+		minDigits = int(hr_min[1])
+		#handle if is PM and needs to be converted to military time
+		if(amPmArray[i] == "pm") :
+			hrDigits = (hrDigits%12) + 12
+		#calulate full digits
+		fullDigits = hrDigits*100+minDigits
+		#convert to string
+		fullHours = str(fullDigits)
+		#add leading 0 if is only 3 characters lon
+		if len(fullHours) == 3 :
+			fullHours = "0" + fullHours
+		formattedHours.append(fullHours)
+	#build final string of opening-closing
+	finalHourString = formattedHours[0]+"-"+formattedHours[1]
+	return finalHourString
 
 
 
@@ -76,7 +105,8 @@ for elt in eatshopHonnoldArray:
 #find actual hours. Find monday by string search and get sibling for hours
 mondayElt = eatshopHonnoldHours.find(text = (re.compile(".Mon."))).parent.next_sibling.next_sibling #first nextsibling gives whitespace
 monHours = military(mondayElt.string)
-
+#print(military("700am - Midnight"))
+#print(military("NOON - 530 pm"))
 
 
 

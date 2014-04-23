57c1f774703db1d76661cdc34e461fd1bf6ea817
Anna Turner <blacksludge@gmail.com>
2014-04-23 10:03:27 -0700

    Scrapes Frary snack hours successfully. Edits to scraper file

--- scraper.py
+++ scraper.py
@@ -19,9 +19,18 @@ import database
 # test = soup.find_all('li', text = (re.compile("Tuesday.") or re.compile("Tuesday.")))
 
 
-
-
-
+#Function that goes through arrays of hours for each day and fills in missing days with the 
+#assumption that those days are implied in a hyphen. I.e. a site might have mon-thurs: 8-10
+#so we carry through the hours for Monday, tues, wed, thurs in the hours array.
+"""TODO: deal with something says "closed", or if hours are simply just not listed for that 
+day if it is closed"""
+def fillInHoursDictionary(dictOfDaysToHours, arrayOfDayStrings) :
+	currHourSet = ""
+	for day in arrayOfDayStrings :
+		if day in dictOfDaysToHours.keys()  :
+			currHourSet = dictOfDaysToHours[day]
+		else : 
+			dictOfDaysToHours[day] = currHourSet
 
 #helper method for calculating military time
 #returns an ordered pair of strings
@@ -119,14 +128,8 @@ for day in arrayOfDayStrings :
 		dayHours = military(dayElt.string)
 		dictOfDaysToHours[day] = dayHours
 
-#temporary way of accounting for "Mon - Thurs"
-"""TODO: deal with something says "closed", or if hours are simply just not listed for that day if it is closed"""
-currHourSet = ""
-for day in arrayOfDayStrings :
-	if day in dictOfDaysToHours.keys()  :
-		currHourSet = dictOfDaysToHours[day]
-	else : 
-		dictOfDaysToHours[day] = currHourSet
+# Fill in the rest of the days
+fillInHoursDictionary(dictOfDaysToHours, arrayOfDayStrings)
 
 #now I need to know how to update the database on the server based on this info. hey update statements? 
 #def updateDatabase(nameToUpdate,dayToUpdate,newHours,timeStamp):
@@ -161,14 +164,8 @@ for day in arrayOfDayStrings :
 		dayHours = military(dayElt.string)
 		dictOfDaysToHours[day] = dayHours
 
-#temporary way of accounting for "Mon - Thurs"
-"""TODO: deal with something says "closed", or if hours are simply just not listed for that day if it is closed"""
-currHourSet = ""
-for day in arrayOfDayStrings :
-	if day in dictOfDaysToHours.keys()  :
-		currHourSet = dictOfDaysToHours[day]
-	else : 
-		dictOfDaysToHours[day] = currHourSet
+#Fill in rest of days
+fillInHoursDictionary(dictOfDaysToHours, arrayOfDayStrings)
 
 #now I need to know how to update the database on the server based on this info. hey update statements? 
 #def updateDatabase(nameToUpdate,dayToUpdate,newHours,timeStamp):
@@ -194,6 +191,9 @@ poDiningURL = "http://www.pomona.edu/administration/dining/facilities-hours/hour
 poDiningHTML = urlopen(poDiningURL).read()
 poDiningSoup = BeautifulSoup(poDiningHTML)
 
+
+
+#FRANK
 poDiningFrank = poDiningSoup.find('h2', text = (re.compile("Frank")))
 
 #get mon-thurs section with by finding next next sibling (next sibling is white space)
@@ -201,40 +201,236 @@ poDiningFrankHoursWeek = poDiningFrank.next_sibling.next_sibling
 poDiningFrankHoursWeekend = poDiningFrankHoursWeek.next_sibling.next_sibling
 
 dictOfDaysToHours.clear()
+
+#Doing Week
 for day in arrayOfDayStrings : 
 	dayRegex = ".*" + day + ".*"
+
 	#if the name of the day is explicitly here, then access hours
-	desiredSection = poDiningFrankHours.find(text = re.compile(dayRegex))
-	if desiredSection : 
-		if "closed" in desiredSection.lower() :
-			#TODO: set to closed
-			break
+	desiredWeekSection = poDiningFrankHoursWeek.find(text = re.compile(dayRegex))
+	if desiredWeekSection : 
+		if "closed" in desiredWeekSection.lower() :
+			dictOfDaysToHours[day] = "Closed"
 		else: 
-			breakfast = desiredSection.parent.parent.next_sibling.next_sibling.next_sibling.next_sibling #first nextsibling gives whitespace
+			#Military expects am and pm to show up in the time. Because the website for Frank and Frary
+			#Doesn't include am and pm in their times we add it in here. We do this by splitting via regex
+			#on the dash and reconstructing each string.
+
+			#Breakfast
+			breakfast = desiredWeekSection.parent.parent.next_sibling.next_sibling.next_sibling.next_sibling #first nextsibling gives whitespace
 			breakfastTime = breakfast.split()[1]
+			breakfastDeconstruct = breakfastTime.split('-')
+			breakfastStart = breakfastDeconstruct[0] + 'am' + '-'
+			breakfastEnd = breakfastDeconstruct[1] + 'pm'
+			breakfastTime = breakfastStart + breakfastEnd
+			breakfastTime = military(breakfastTime)
+
+			#Lunch hours
 			lunch = breakfast.next_sibling.next_sibling
+			lunchTime = lunch.split()[1]
+			lunchDeconstruct = lunchTime.split('-')
+			lunchStart = lunchDeconstruct[0] + 'am' + '-'
+			lunchEnd = lunchDeconstruct[1] + 'pm'
+			lunchTime = lunchStart + lunchEnd
+			lunchTime = military(lunchTime)
+
+			#Dinner hours
 			dinner = lunch.next_sibling.next_sibling
+			dinnerTime = dinner.split()[1]
+			dinnerDeconstruct = dinnerTime.split('-')
+			dinnerStart = dinnerDeconstruct[0] + 'pm' + '-'
+			dinnerEnd = dinnerDeconstruct[1] + 'pm'
+			dinnerTime = dinnerStart + dinnerEnd
+			dinnerTime = military(dinnerTime)
+
+			#Concatenate and add to array
+			frankHoursWeek = breakfastTime + '%' + lunchTime + '%' + dinnerTime
+			dictOfDaysToHours[day] = frankHoursWeek
+
+	#Do Weekend now
+	desiredWeekendSection = poDiningFrankHoursWeekend.find(text = re.compile(dayRegex))
+	if desiredWeekendSection :
+		if "closed" in desiredWeekendSection.lower() :
+			dictOfDaysToHours[day] = "Closed"
+		else: 
+			#Same thing as above with brunch and dinner
 			
-			###UGH but i'm going to have to deal with brunch and dinner so this for loop might not work.
-			#i might just have to do the weekend days separately
+			#Brunch
+			brunch = desiredWeekendSection.parent.parent.next_sibling.next_sibling.next_sibling.next_sibling #first nextsibling gives whitespace
+			brunchTime = brunch.split()[1]
+			brunchDeconstruct = brunchTime.split('-')
+			brunchStart = brunchDeconstruct[0] + 'am' + '-'
+			brunchEnd = brunchDeconstruct[1] + 'pm'
+			brunchTime = brunchStart + brunchEnd
+			brunchTime = military(brunchTime)
+
+			#Dinner
+			dinner = brunch.next_sibling.next_sibling
+			dinnerTime = dinner.split()[1]
+			dinnerDeconstruct = dinnerTime.split('-')
+			dinnerStart = dinnerDeconstruct[0] + 'pm' + '-'
+			dinnerEnd = dinnerDeconstruct[1] + 'pm'
+			dinnerTime = dinnerStart + dinnerEnd
+			dinnerTime = military(dinnerTime)
+
+			#Concatenate and add to array
+			frankHoursWeekend = brunchTime + '%' + dinnerTime
+			dictOfDaysToHours[day] = frankHoursWeekend
+
+#Fill in rest of days and hours array
+fillInHoursDictionary(dictOfDaysToHours, arrayOfDayStrings)
+#Update Database
+for i in range(len(arrayOfDayStrings)) :
+	database.updateDatabase("Frank Dining Hall", databaseDayStrings[i], dictOfDaysToHours[arrayOfDayStrings[i]], "0")
 
 
 
 
-database.updateDatabase("Frank Dining Hall", "monday", "0200-0600%0700-1200", "0")
-database.updateDatabase("Frank Dining Hall", "tuesday", "0200-0600%0700-1200", "0")
-database.updateDatabase("Frank Dining Hall", "wednesday", "0200-0600%0700-1200", "0")
-database.updateDatabase("Frank Dining Hall", "thursday", "0200-0600%0700-1200", "0")
-database.updateDatabase("Frank Dining Hall", "friday", "0200-0600%0700-1200", "0")
-database.updateDatabase("Frank Dining Hall", "saturday", "0200-0600%0700-1200", "0")
-database.updateDatabase("Frank Dining Hall", "sunday", "0200-0600%0700-1200", "0")
-database.updateDatabase("Frary Dining Hall", "monday", "0200-0600%0700-1200", "0")
-database.updateDatabase("Frary Dining Hall", "tuesday", "0200-0600%0700-1200", "0")
-database.updateDatabase("Frary Dining Hall", "wednesday", "0200-0600%0700-1200", "0")
-database.updateDatabase("Frary Dining Hall", "thursday", "0200-0600%0700-1200", "0")
-database.updateDatabase("Frary Dining Hall", "friday", "0200-0600%0700-1200", "0")
-database.updateDatabase("Frary Dining Hall", "saturday", "0200-0600%0700-1200", "0")
-database.updateDatabase("Frary Dining Hall", "sunday", "0200-1200", "0")
+
+
+#FRARY
+poDiningFrary = poDiningSoup.find('h2', text = (re.compile("Frary")))
+
+#get mon-thurs section with by finding next next sibling (next sibling is white space)
+poDiningFraryHoursWeek = poDiningFrary.next_sibling.next_sibling
+poDiningFraryHoursWeekend = poDiningFraryHoursWeek.next_sibling.next_sibling
+poDiningFraryHoursSnack = poDiningFraryHoursWeekend.next_sibling.next_sibling.next_sibling.next_sibling
+
+dictOfDaysToHours.clear()
+
+#Doing Week
+for day in arrayOfDayStrings : 
+	dayRegex = ".*" + day + ".*"
+
+	#if the name of the day is explicitly here, then access hours
+	desiredWeekSection = poDiningFraryHoursWeek.find(text = re.compile(dayRegex))
+	if desiredWeekSection : 
+		if "closed" in desiredWeekSection.lower() :
+			dictOfDaysToHours[day] = "Closed"
+		else: 
+			#Military expects am and pm to show up in the time. Because the website for Frary and Frary
+			#Doesn't include am and pm in their times we add it in here. We do this by splitting via regex
+			#on the dash and reconstructing each string.
+
+			#Breakfast
+			breakfast = desiredWeekSection.parent.parent.next_sibling.next_sibling#first nextsibling gives whitespace
+			breakfastTime = breakfast.split()[1]
+			breakfastDeconstruct = breakfastTime.split('-')
+			breakfastStart = breakfastDeconstruct[0] + 'am' + '-'
+			breakfastEnd = breakfastDeconstruct[1] + 'am'
+			breakfastTime = breakfastStart + breakfastEnd
+			breakfastTime = military(breakfastTime)
+
+			#Lunch hours
+			lunch = breakfast.next_sibling.next_sibling
+			lunchTime = lunch.split()[1]
+			lunchDeconstruct = lunchTime.split('-')
+			lunchStart = lunchDeconstruct[0] + 'am' + '-'
+			lunchEnd = lunchDeconstruct[1] + 'pm'
+			lunchTime = lunchStart + lunchEnd
+			lunchTime = military(lunchTime)
+
+			#Dinner hours
+			dinner = lunch.next_sibling.next_sibling
+			dinnerTime = dinner.split()[1]
+			dinnerDeconstruct = dinnerTime.split('-')
+			dinnerStart = dinnerDeconstruct[0] + 'pm' + '-'
+			dinnerEnd = dinnerDeconstruct[1] + 'pm'
+			dinnerTime = dinnerStart + dinnerEnd
+			dinnerTime = military(dinnerTime)
+
+			#Concatenate and add to array
+			fraryHoursWeek = breakfastTime + '%' + lunchTime + '%' + dinnerTime
+			dictOfDaysToHours[day] = fraryHoursWeek
+
+	#Do Weekend now
+	desiredWeekendSection = poDiningFraryHoursWeekend.find(text = re.compile(dayRegex))
+	if desiredWeekendSection :
+		if "closed" in desiredWeekendSection.lower() :
+			dictOfDaysToHours[day] = "Closed"
+		else: 
+			#Same thing as above with continental bfast, brunch and dinner
+			cBreakfast = desiredWeekendSection.parent.parent.next_sibling.next_sibling #first nextsibling gives whitespace
+			cBreakfastTime = cBreakfast.split()[2]
+			cBreakfastDeconstruct = cBreakfastTime.split('-')
+			cBreakfastStart = cBreakfastDeconstruct[0] + 'am' + '-'
+			cBreakfastEnd = cBreakfastDeconstruct[1] + 'pm'
+			cBreakfastTime = cBreakfastStart + cBreakfastEnd
+			cBreakfastTime = military(cBreakfastTime)
+
+			#Brunch
+			brunch = cBreakfast.next_sibling.next_sibling
+			brunchTime = brunch.split()[1]
+			brunchDeconstruct = brunchTime.split('-')
+			brunchStart = brunchDeconstruct[0] + 'am' + '-'
+			brunchEnd = brunchDeconstruct[1] + 'pm'
+			brunchTime = brunchStart + brunchEnd
+			brunchTime = military(brunchTime)
+
+			#Dinner
+			dinner = brunch.next_sibling.next_sibling
+			dinnerTime = dinner.split()[1]
+			dinnerDeconstruct = dinnerTime.split('-')
+			dinnerStart = dinnerDeconstruct[0] + 'pm' + '-'
+			dinnerEnd = dinnerDeconstruct[1] + 'pm'
+			dinnerTime = dinnerStart + dinnerEnd
+			dinnerTime = military(dinnerTime)
+
+			#Concatenate and add to array
+			fraryHoursWeekend = cBreakfastTime + '%' + brunchTime + '%' + dinnerTime
+			dictOfDaysToHours[day] = fraryHoursWeekend
+			#print dictOfDaysToHours
+
+#Doing Frary Snack Separately
+#Snack
+snack = poDiningFraryHoursSnack.find("strong").next_sibling
+snackTime = snack.split()[0]
+snackDeconstruct = snackTime.split('-')
+snackStart = snackDeconstruct[0] + 'pm' + '-'
+snackEnd = snackDeconstruct[0].replace(',','') + 'pm'
+snackTime = snackStart + snackEnd
+snackTime = military(snackTime)
+
+
+#Fill in rest of days and hours array
+fillInHoursDictionary(dictOfDaysToHours, arrayOfDayStrings)
+
+#Tack on snack hours
+dictOfDaysToHours["Sun"] = dictOfDaysToHours["Sun"] + '%' + str(snackTime)
+dictOfDaysToHours["Mon"] = dictOfDaysToHours["Mon"] + '%' + str(snackTime)
+dictOfDaysToHours["Tues"] = dictOfDaysToHours["Tues"] + '%' + str(snackTime)
+dictOfDaysToHours["Wed"] = dictOfDaysToHours["Wed"] + '%' + str(snackTime)
+
+#Update Database
+for i in range(len(arrayOfDayStrings)) :
+	database.updateDatabase("Frary Dining Hall", databaseDayStrings[i], dictOfDaysToHours[arrayOfDayStrings[i]], "0")
+
+
+
+
+
+
+
+			
+
+
+
+
+
+# database.updateDatabase("Frank Dining Hall", "monday", "0200-0600%0700-1200", "0")
+# database.updateDatabase("Frank Dining Hall", "tuesday", "0200-0600%0700-1200", "0")
+# database.updateDatabase("Frank Dining Hall", "wednesday", "0200-0600%0700-1200", "0")
+# database.updateDatabase("Frank Dining Hall", "thursday", "0200-0600%0700-1200", "0")
+# database.updateDatabase("Frank Dining Hall", "friday", "0200-0600%0700-1200", "0")
+# database.updateDatabase("Frank Dining Hall", "saturday", "0200-0600%0700-1200", "0")
+# database.updateDatabase("Frank Dining Hall", "sunday", "0200-0600%0700-1200", "0")
+# database.updateDatabase("Frary Dining Hall", "monday", "0200-0600%0700-1200", "0")
+# database.updateDatabase("Frary Dining Hall", "tuesday", "0200-0600%0700-1200", "0")
+# database.updateDatabase("Frary Dining Hall", "wednesday", "0200-0600%0700-1200", "0")
+# database.updateDatabase("Frary Dining Hall", "thursday", "0200-0600%0700-1200", "0")
+# database.updateDatabase("Frary Dining Hall", "friday", "0200-0600%0700-1200", "0")
+# database.updateDatabase("Frary Dining Hall", "saturday", "0200-0600%0700-1200", "0")
+# database.updateDatabase("Frary Dining Hall", "sunday", "0200-1200", "0")
 
 
 

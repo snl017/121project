8f7a938890489d0793b6c00b2591abc247ad37b9
Anna Turner <blacksludge@gmail.com>
2014-04-26 15:36:38 -0700

    added detail cell view files

---
+++ iNeed/detailCell.h
@@ -0,0 +1,13 @@
+//
+//  detailCell.h
+//  iNeed
+//
+//  Created by Anna Turner on 4/26/14.
+//  Copyright (c) 2014 121. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface detailCell : UITableViewCell
+
+@end
---
+++ iNeed/detailCell.m
@@ -0,0 +1,29 @@
+//
+//  detailCell.m
+//  iNeed
+//
+//  Created by Anna Turner on 4/26/14.
+//  Copyright (c) 2014 121. All rights reserved.
+//
+
+#import "detailCell.h"
+
+@implementation detailCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end

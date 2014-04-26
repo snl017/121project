6f102cd8cfc7b345d393dca31e51c40649a308b9
Anna Turner <blacksludge@gmail.com>
2014-04-26 14:32:22 -0700

    Added global functions file

---
+++ iNeed/iNeed/GlobalFunctions.h
@@ -0,0 +1,16 @@
+//
+//  GlobalFunctions.h
+//  iNeed
+//
+//  Created by Anna Turner on 4/26/14.
+//  Copyright (c) 2014 121. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GlobalFunctions : NSObject
+
+//Function lets us use hex colors
++(UIColor*)colorWithHexString:(NSString*)hex;
+
+@end
---
+++ iNeed/iNeed/GlobalFunctions.m
@@ -0,0 +1,51 @@
+//
+//  GlobalFunctions.m
+//  iNeed
+//
+//  Created by Anna Turner on 4/26/14.
+//  Copyright (c) 2014 121. All rights reserved.
+//
+
+#import "GlobalFunctions.h"
+
+@implementation GlobalFunctions
+
+//Taken from http://stackoverflow.com/questions/6207329/how-to-set-hex-color-code-for-background
+//Lets us set colors with hex strings
+
++(UIColor*)colorWithHexString:(NSString*)hex{
+    NSString *cString = [[hex stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] uppercaseString];
+    
+    // String should be 6 or 8 characters
+    if ([cString length] < 6) return [UIColor grayColor];
+    
+    // strip 0X if it appears
+    if ([cString hasPrefix:@"0X"]) cString = [cString substringFromIndex:2];
+    
+    if ([cString length] != 6) return  [UIColor grayColor];
+    
+    // Separate into r, g, b substrings
+    NSRange range;
+    range.location = 0;
+    range.length = 2;
+    NSString *rString = [cString substringWithRange:range];
+    
+    range.location = 2;
+    NSString *gString = [cString substringWithRange:range];
+    
+    range.location = 4;
+    NSString *bString = [cString substringWithRange:range];
+    
+    // Scan values
+    unsigned int r, g, b;
+    [[NSScanner scannerWithString:rString] scanHexInt:&r];
+    [[NSScanner scannerWithString:gString] scanHexInt:&g];
+    [[NSScanner scannerWithString:bString] scanHexInt:&b];
+    
+    return [UIColor colorWithRed:((float) r / 255.0f)
+                           green:((float) g / 255.0f)
+                            blue:((float) b / 255.0f)
+                           alpha:1.0f];
+}
+
+@end

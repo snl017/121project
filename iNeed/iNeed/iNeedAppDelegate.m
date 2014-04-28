c132336d2ac5d805b61871d9ad010977a00c91e1
Anna Turner <blacksludge@gmail.com>
2014-04-28 11:15:26 -0700

    Commented out test adds to database

--- iNeed/iNeed/iNeedAppDelegate.m
+++ iNeed/iNeed/iNeedAppDelegate.m
@@ -89,23 +89,23 @@
 //    //Empty the database if necessary
     [PlaceDatabase emptyDatabase];
     
-    //Save some test places to database
-    [PlaceDatabase saveItemWithPlace:fraryDining andSpecificCategory:DiningHallNarrow andBroadCategory:FoodBroad];
-    [PlaceDatabase saveItemWithPlace:frankDining andSpecificCategory:DiningHallNarrow andBroadCategory:FoodBroad];
-    [PlaceDatabase saveItemWithPlace:campusSafety andSpecificCategory:SafetyHealth andBroadCategory:SafetyHealth];
-    [PlaceDatabase saveItemWithPlace:studyAbroad andSpecificCategory:NonAcademicRCNarrow andBroadCategory:AcademicRCNarrow];
-    [PlaceDatabase saveItemWithPlace:coopFountain andSpecificCategory:EateryGroceryNarrow andBroadCategory:FoodBroad];
-    [PlaceDatabase savePlace:coopStore.name withSpecificCategory:StoresNarrow andBroadCategory:LivingOnCampusBroad];
-    [PlaceDatabase saveItemWithPlace:coopStore andSpecificCategory:EateryGroceryNarrow andBroadCategory:FoodBroad];
-    [PlaceDatabase saveItemWithPlace:writingCenter andSpecificCategory:AcademicRCNarrow andBroadCategory:ResourceCentersOfficesBroad];
-    [PlaceDatabase saveItemWithPlace:its andSpecificCategory:ServicesNarrow andBroadCategory:LivingOnCampusBroad];
-    [PlaceDatabase saveItemWithPlace:honnoldcafe andSpecificCategory:EateryGroceryNarrow andBroadCategory:FoodBroad];
-    
-    
-    
-}
-							
-- (void)applicationWillResignActive:(UIApplication *)application
+//    //Save some test places to database
+//    [PlaceDatabase saveItemWithPlace:fraryDining andSpecificCategory:DiningHallNarrow andBroadCategory:FoodBroad];
+//    [PlaceDatabase saveItemWithPlace:frankDining andSpecificCategory:DiningHallNarrow andBroadCategory:FoodBroad];
+//    [PlaceDatabase saveItemWithPlace:campusSafety andSpecificCategory:SafetyHealth andBroadCategory:SafetyHealth];
+//    [PlaceDatabase saveItemWithPlace:studyAbroad andSpecificCategory:NonAcademicRCNarrow andBroadCategory:AcademicRCNarrow];
+//    [PlaceDatabase saveItemWithPlace:coopFountain andSpecificCategory:EateryGroceryNarrow andBroadCategory:FoodBroad];
+//    [PlaceDatabase savePlace:coopStore.name withSpecificCategory:StoresNarrow andBroadCategory:LivingOnCampusBroad];
+//    [PlaceDatabase saveItemWithPlace:coopStore andSpecificCategory:EateryGroceryNarrow andBroadCategory:FoodBroad];
+//    [PlaceDatabase saveItemWithPlace:writingCenter andSpecificCategory:AcademicRCNarrow andBroadCategory:ResourceCentersOfficesBroad];
+//    [PlaceDatabase saveItemWithPlace:its andSpecificCategory:ServicesNarrow andBroadCategory:LivingOnCampusBroad];
+//    [PlaceDatabase saveItemWithPlace:honnoldcafe andSpecificCategory:EateryGroceryNarrow andBroadCategory:FoodBroad];
+//    
+//    
+//    
+//}
+//							
+//- (void)applicationWillResignActive:(UIApplication *)application
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
